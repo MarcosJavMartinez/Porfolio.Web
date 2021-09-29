3e5d1380315a07bb931958bb6ae96e6efa209897
Marcos <marcosjavrmartinez@gmail.com>
2021-09-29 17:08:13 -0300

    Collapse

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+const botonA = document.getElementById("collapseA");
+const contenidoA = document.getElementById("contentA");
+let flagA = false;
 
-const boton = document.getElementById("collapse");
-const contenido = document.getElementById("content");
-let flag = false 
-boton.addEventListener("click", () => {
-    if(flag === false){
-        contenido.classList.add("collapsed");
-        //flag = true;
-    }else{
-        contenido.classList.remove("collapsed");
-        //flag = false;
-    }
-   flag = !flag 
-    
+const botonB = document.getElementById("collapseB");
+const contenidoB = document.getElementById("contentB");
+let flagB = false;
+
+const botonC = document.getElementById("collapseC");
+const contenidoC = document.getElementById("contentC");
+let flagC = false;
+
+botonA.addEventListener("click", () => {
+	if(flagA === false){
+		contenidoA.classList.add("collapsed");
+	}else{
+		contenidoA.classList.remove("collapsed");
+	}
+	flagA = !flagA;
+});
+
+botonB.addEventListener("click", () => {
+	if(flagB === false){
+		contenidoB.classList.add("collapsed");
+	}else{
+		contenidoB.classList.remove("collapsed");
+	}
+	flagB = !flagB;
+});
+
+botonC.addEventListener("click", () => {
+	if(flagC === false){
+		contenidoC.classList.add("collapsed");
+	}else{
+		contenidoC.classList.remove("collapsed");
+	}
+	flagC = !flagC;
 });
